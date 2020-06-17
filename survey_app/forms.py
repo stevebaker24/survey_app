993a4f110950d6e0b5788acf35eaa22564e14b32
Steve Baker <steve@smigg.co.uk>
2020-06-17 14:41:59 +0100

    added navbar and pages for the different report types

--- survey_app/forms.py
+++ survey_app/forms.py
@@ -1,11 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectMultipleField
+from wtforms import StringField, SubmitField, SelectMultipleField, SelectField
 from wtforms.validators import DataRequired
 
 
-class LoginForm(FlaskForm):
+class RagForm(FlaskForm):
     trust = StringField('Organisation ID', validators=[DataRequired()])
     locality = StringField('Breakdown', validators=[DataRequired()])
-    score_type = SelectMultipleField('Score Type', choices=[('pos', 'Positive'), ('neu', 'Neutral'), ('neg', 'Negative')],
-                                     validators=[DataRequired()])
+    score_type = SelectField('Score Type', choices=[('pos', 'Positive'), ('neu', 'Neutral'), ('neg', 'Negative')])
+    submit = SubmitField('Submit')
+
+
+class FreeTextForm(FlaskForm):
+    trust = StringField('Organisation ID', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class FreqForm(FlaskForm):
+    trust = StringField('Organisation ID', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- survey_app/routes.py
+++ survey_app/routes.py
@@ -2,22 +2,71 @@
 
 from flask import render_template, send_file
 from survey_app import app
-from survey_app.forms import LoginForm
+from survey_app.forms import RagForm, FreqForm, FreeTextForm
 import sys
 
 sys.path.append('C:\\Users\\steve.baker\\PycharmProjects\\survey_platform\\survey_platform')
 import survey_platform as sp
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET'])
 def index():
-    form = LoginForm()
+    return render_template('index.html')
+
+
+@app.route('/rag', methods=['GET', 'POST'])
+def rag():
+    form = RagForm()
     if form.validate_on_submit():
 
-        survey = sp.Survey('TESTSURVEY', 'SPECIALSURVEY', 'survey_app/static/questions.csv',
-                           'survey_app/static/responses.parquet', 'survey_app/static/sample.parquet')
+        survey = sp.Survey(name='TESTSURVEY',
+                           survey_type='SPECIALSURVEY',
+                           questions_path='survey_app/static/questions.csv',
+                           responses_path='survey_app/static/responses.parquet',
+                           sample_path='survey_app/static/sample.parquet')
 
-        return send_file(survey.combined.get_summary(form.trust.data, form.locality.data,
-                                                     form.score_type.data), as_attachment=True)
+        return send_file(sp.get_rag(survey,
+                                    seperator=form.trust.data,
+                                    group_by=form.locality.data,
+                                    questions=survey.questions),
+                         as_attachment=True)
 
-    return render_template('index.html', form=form)
+    return render_template('rag.html', form=form)
+
+
+@app.route('/freetext', methods=['GET', 'POST'])
+def text():
+    form = FreeTextForm()
+    if form.validate_on_submit():
+
+        survey = sp.Survey(name='TESTSURVEY',
+                           survey_type='SPECIALSURVEY',
+                           questions_path='survey_app/static/questions.csv',
+                           responses_path='survey_app/static/responses.parquet',
+                           sample_path='survey_app/static/sample.parquet')
+
+        return send_file(sp.get_freetext(combined=survey.combined.df,
+                                    trust=form.trust.data,
+                                    questions=survey.questions),
+                         as_attachment=True)
+
+    return render_template('freetext.html', form=form)
+
+
+@app.route('/freq', methods=['GET', 'POST'])
+def freq():
+    form = FreqForm()
+    if form.validate_on_submit():
+
+        survey = sp.Survey(name='TESTSURVEY',
+                           survey_type='SPECIALSURVEY',
+                           questions_path='survey_app/static/questions.csv',
+                           responses_path='survey_app/static/responses.parquet',
+                           sample_path='survey_app/static/sample.parquet')
+
+        return send_file(sp.get_freq_tables(combined=survey.combined.df,
+                                    trust=form.trust.data,
+                                    questions=survey.questions),
+                         as_attachment=True)
+
+    return render_template('freq.html', form=form)
